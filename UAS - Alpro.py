5eb74b03ff7b4cd8134cc229b3e4badaa4ab5f39
Weiss_GCM <darma20060311@gmail.com>
2025-01-20 08:40:23 +0700

    Add files via upload

---
+++ UAS - Alpro.py
@@ -0,0 +1,70 @@
+#Function | Digunakan untuk Menampilkan Menu serta Input untuk memilih pilihan yang muncul pada daftar
+def menu():
+    print("Daftar Pilihan: ")
+    print("1. Tambah Barang")
+    print("2. Lihat Daftar")
+    print("3. Total Seluruh")
+    opsi = input("Pilih nomor pada daftar: ")
+    return opsi
+
+#Function | Digunakan untuk Menghitung Total akhir dengan jumlah barang (qty) dikalikan dengan harga (pcy)
+def qty_ex_pcy(qty, pcy):
+    return qty * pcy
+
+#Function & Dictionary | Digunakan untuk Menambah Barang beserta memasukan barang kedalam dictionary menuju List
+def plus_tag():
+    item_tag = input("Masukan nama barang, press enter jika tidak ada barang: ")
+    if item_tag.lower() == '':
+        return None
+    pcy_tag = int(input(f"Masukan harga barang {item_tag}  : "))
+    qty_tag = int(input(f"Jumlah barang {item_tag}        : "))
+    item_info = {
+        'Nama Barang': item_tag,
+        'Harga Barang': pcy_tag,
+        'Jumlah Barang': qty_tag,
+        'Total': qty_ex_pcy(qty_tag, pcy_tag) 
+    }
+    d_list.append(item_info)
+
+#Function | Digunakan untuk Menampilkan List Barang
+def show_list():
+    if not d_list:
+        print("Ga ada data yang dimasukin")
+    else:
+        print("List Barang & Harga")
+        print(f"{'Nama Barang':<20}{'Harga Barang':<20}{'Jumlah Barang':<20}{'Total':<20}")
+        print("-" * 67)
+        for list_item in d_list:
+            print(f"{list_item['Nama Barang']:<20}{list_item['Harga Barang']:<20}{list_item['Jumlah Barang']:<20}{list_item['Total']:<20}")
+
+#Function | Digunakan untuk Menampilkan Total Keseluruhan serta menghitung jumlah kembalian 
+def total_result():
+    if not d_list:
+        print("Tidak ada barang yang dimasukan")
+    else:
+        total = sum(list_item['Total'] for list_item in d_list)
+        print(f"{'Nama Barang':<20}{'Harga Barang':<20}{'Jumlah Barang':<20}{'Total':<20}")
+        print("-" * 67)
+        for list_item in d_list:
+            print(f"{list_item['Nama Barang']:<20}{list_item['Harga Barang']:<20}{list_item['Jumlah Barang']:<20}{list_item['Total']:<20}")
+        print("-" * 67)
+        print(f"{'Total Keseluruhan':<20}{'':<20}{'':<20}{total:<20}")
+        pembayaran = int(input("Jumlah pembayaran : Rp. "))
+        kembalian = pembayaran - total 
+        print(f"kembalian  : Rp.{kembalian:,.2f}")
+
+#List | Data yang dimasukan pada input Nama barang, Harga barang, Jumlah barang akan disimpan disini
+d_list = []
+
+#Looping | Menggunakan While True, agar program dapat diulang hingga user memilih angka 3 (Hasil Akhir) apabila selain 1-3 program akan berhenti
+while True:
+    opsi = menu()
+    if opsi == '1':
+        plus_tag()
+    elif opsi == '2':
+        show_list()
+    elif opsi == '3':
+        total_result()
+        break
+    else:
+        break
